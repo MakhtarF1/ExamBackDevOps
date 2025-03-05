64006f4a968c240a4099320cd2ef08059ca3e13b
MakhtarF1 <fallmakh026@gmail.com>
2025-03-05 16:05:43 +0000

    Ajout de la configuration ESLint et du pipeline CI/CD

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,43 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import tseslint from "@typescript-eslint/eslint-plugin";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node, // Ajoutez les variables globales de Node.js
+        ...globals.node, // Ajoute les variables globales de Node.js
       },
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginJs.configs.recommended, // Recommandations de ESLint pour JS
+
+  // Configuration des règles TypeScript
+  {
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/explicit-module-boundary-types": "off", // Exemples de règles spécifiques pour TypeScript
+    },
+  },
+
+  // Configuration des règles React
+  {
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      "react/jsx-uses-react": "off", // Exemple de règle personnalisée pour React
+      "react/jsx-uses-vars": "off",
+    },
+  },
+
+  // Définir des règles personnalisées
   {
     rules: {
       "constructor-super": "off", // Désactive la règle constructor-super
